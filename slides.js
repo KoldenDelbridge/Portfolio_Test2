c36b15fcc3c1c0c34bd211624a6931c44d8f0c2c
KoldenDelbridge <koldendelbridge@gmail.com>
2025-06-24 14:21:36 -0700

    kgkg

---
+++ slides.js
@@ -0,0 +1,21 @@
+const slides = [
+    {
+        tag: "Packaging",
+        marquee: "Krillaz Protein Can Label",
+        image: "krilaz_mockup.png",
+    },
+
+    {
+        tag: "Packaging",
+        marquee: "Little Inferno Hotsauce Label",
+        image: "slide_2",
+    },
+
+    {
+        tag: "Marketing",
+        marquee: "Free Rein Social Media Posts",
+        image: "slide_3",
+    },
+
+
+]
